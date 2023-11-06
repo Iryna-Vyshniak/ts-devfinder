b19e12ad17bf864dd4e1727f762b904356beaef1
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-11-06 20:26:40 +0200

    add mock - default user obj

---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { LocalGithubUser } from "../types";
+
+export const defaultUser: LocalGithubUser = {
+    "login": "Iryna V-N",
+    "avatar": "https://avatars.githubusercontent.com/u/111734415?s=400&u=7532c9eff203491cc02069116563b2bd05884f07&v=4",
+    "name": "Iryna-Vyshniak",
+    "company": "Famouse future company ✌️",
+    "blog": "Not yet",
+    "location": "Ukraine, Kyiv",
+    "bio": "React | Next | React Native Developer",
+    "twitter": "Miss_V_N_",
+    "repos": 60,
+    "followers": 94,
+    "following": 35,
+    "created": "2022-08-22T16:18:45Z",
+}
