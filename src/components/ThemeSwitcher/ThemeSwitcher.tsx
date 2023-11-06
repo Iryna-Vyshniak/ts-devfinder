08104c47f845078b2fd61227f0807c45b330eda9
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-11-06 22:17:57 +0200

    Implement ThemeSwitcher component for toggling between light and dark themes with Sun and Moon icons, updating body attribute

---
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import MoonIcon from 'assets/icon-moon.svg?react';
+import SunIcon from 'assets/icon-sun.svg?react';
+
+import styles from './ThemeSwitcher.module.scss';
+
+const ThemeSwitcher = () => {
+  const [isDark, setDark] = useState(false);
+  const textTheme = isDark ? 'Light' : 'Dark';
+  const ThemeIcon = isDark ? SunIcon : MoonIcon;
+
+  useEffect(() => {
+    document.body.setAttribute('data-theme', isDark ? 'dark' : 'light');
+  }, [isDark]);
+
+  const toggleTheme = () => {
+    setDark(!isDark);
+  };
+
+  return (
+    <div className={styles.switcher} onClick={toggleTheme}>
+      <span>{textTheme}</span>
+      <ThemeIcon className={styles.icon} />
+    </div>
+  );
+};
+
+export default ThemeSwitcher;
