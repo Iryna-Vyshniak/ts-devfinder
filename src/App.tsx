f3410dfc8416425aeafe3954635234953d14f94d
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-11-07 02:21:10 +0200

    Create and complete Search component, implemented user fetching and display in the App component with default state and error handling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
+import { useState } from 'react';
+import { defaultUser } from './mock';
+
+import Search from './components/Search/Search';
 import Container from './components/Container/Container';
 import Header from './components/Header/Header';
+import { GithubError, GithubUser, LocalGithubUser } from 'types';
+import { BASE_URL } from './shared/constants/constants';
+import { isGithubUser } from './shared/utils/typeguards';
+import { extractLocalUser } from './shared/utils/exract-local-user';
+import { UserCard } from './components/UserCard';
 
 function App() {
+  const [user, setUser] = useState<LocalGithubUser | null>(defaultUser);
+
+  const fetchUser = async (username: string) => {
+    const url = BASE_URL + username;
+
+    const res = await fetch(url);
+    const user = (await res.json()) as GithubUser | GithubError;
+    console.log('user: ', user);
+
+    if (isGithubUser(user)) {
+      setUser(extractLocalUser(user));
+    } else {
+      setUser(null);
+    }
+  };
+
   return (
     <Container>
       <Header />
+      <Search onSubmit={fetchUser} hasError={!user} />
+      {user && <UserCard {...user} />}
     </Container>
   );
 }
---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,45 @@
+import SearchIcon from '../../assets/icon-search.svg?react';
+import Button from '../../components/Button/Button';
+import styles from './Search.module.scss';
+
+interface SearchProps {
+  hasError: boolean;
+  onSubmit: (text: string) => void;
+}
+
+type FormFields = {
+  username: HTMLInputElement;
+};
+
+const Search = ({ hasError, onSubmit }: SearchProps) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement & FormFields>) => {
+    e.preventDefault();
+    const text = e.currentTarget.username.value;
+
+    if (text.trim()) {
+      onSubmit(text);
+      e.currentTarget.reset();
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} autoComplete='off'>
+      <div className={styles.search}>
+        <label htmlFor='search' className={styles.label}>
+          <SearchIcon />
+        </label>
+        <input
+          type='text'
+          name='username'
+          id='search'
+          placeholder='Search GitHub username...'
+          className={styles.textField}
+        />
+        {hasError && <p className={styles.error}>No result</p>}
+        <Button>Search</Button>
+      </div>
+    </form>
+  );
+};
+
+export default Search;
