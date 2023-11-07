bafe9f4be19d172ae6dc995c4b21542e2ee18c42
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-11-07 02:18:29 +0200

    Create and complete info info component

--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -5,7 +5,7 @@ import SunIcon from '../../assets/icon-sun.svg?react';
 import styles from './ThemeSwitcher.module.scss';
 
 const ThemeSwitcher = () => {
-  const [isDark, setDark] = useState(false);
+  const [isDark, setDark] = useState(true);
   const textTheme = isDark ? 'Light' : 'Dark';
   const ThemeIcon = isDark ? SunIcon : MoonIcon;
 
